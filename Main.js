a717d200d46ff2ebf5b5d15cc864ca481b1f8f5f
MikeyZee <michaelzenkis@hotmail.com>
2017-12-04 00:38:21 +1100

    Added first online help popup

--- Main.js
+++ Main.js
@@ -58,3 +58,10 @@ function Generatechart() {
         }
     })
 }
+
+
+// When the user clicks on div, open the popup
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
