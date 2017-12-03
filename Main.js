dde91bced2a367aa52091a8b7612a90f717b08ba
MikeyZee <michaelzenkis@hotmail.com>
2017-12-03 22:40:21 +1100

    completed javascript calculation

--- Main.js
+++ Main.js
@@ -1,39 +1,49 @@
-const CHART = document.getElementById("LineChart");
-let LineChart= new Chart(CHART, {
-    type:'line',
-    data: {
-        labels: ["january", "February", "March", "April", "May", "June", "July"],
-        datasets: [
-            {
-                label: "My First Database",
-                Fill: true,
-                lineTension: 0.0,
-                backgroundColor: "rgba(75,192,192,0.4)",
-                borderCapStyle: 'butt',
-                borderdashOffser: 0.0,
-                borderJoinStyle: 'niter',
-                pointHoverRadius: 5,
-                pointRadius: 1,
-                data: [12,42,53,62,15,36,12],
-            }
-        ]
-    },
-    options:{
-        scales: {
-            yAxes:[{
-                ticks:{
-                    beginAtZero: true
-                }
-            }]
-        }
+function Generatechart() {
+    var altitudeArray = document.getElementById('altitudes').value.split(',');
+    console.log(altitudeArray);
+    var frequency = document.getElementById("frequency").value;
+    console.log(frequency);
+    console.log(Math.floor(altitudeArray.length/frequency));
+
+    var filteredAltitudeArray = [];
+    var secondsAxis = []
+    for (let index = 0; index < Math.floor(altitudeArray.length/frequency); index++) {
+        filteredAltitudeArray.push(altitudeArray[(frequency*(index+1))-1]);
+        secondsAxis.push(index+1);
     }
-})
+
+    console.log(filteredAltitudeArray);
+    console.log(secondsAxis);
 
 
-    var altitudeArray = document.getElementById('please').value.split(',');
-  //  var iterations = document.getElementById("iterationlog");
-  //  var altitude = document.getElementById("altitudelog");
-   // var iterationsArray = [iterations];
-  //  var altitudeArray = [altitude];
-
-    
+    const CHART = document.getElementById("LineChart");
+    let LineChart= new Chart(CHART, {
+        type:'line',
+        data: {
+            labels: secondsAxis,
+            datasets: [
+                {
+                    label: "My First Database",
+                    Fill: true,
+                    lineTension: 0.0,
+                    backgroundColor: "rgba(75,192,192,0.4)",
+                    borderCapStyle: 'butt',
+                    borderdashOffser: 0.0,
+                    borderJoinStyle: 'niter',
+                    pointHoverRadius: 5,
+                    pointRadius: 1,
+                    data: filteredAltitudeArray,
+                }
+            ]
+        },
+        options:{
+            scales: {
+                yAxes:[{
+                    ticks:{
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    })
+}
